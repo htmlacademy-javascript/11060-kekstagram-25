c51b0be39243520d9fbeca44b98539b2833bc903
kernelidze <kernelidze@gmail.com>
2022-02-28 12:09:06 +0200

    module2-task1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function getRandomInteger (min, max) {
   if (max < min) {
-    let newMax = min;
-    let newMin = max;
+    const newMax = min;
+    const newMin = max;
     max = newMax;
     min = newMin;
   }
 
   if (max < 0 || min < 0) {
-    return console.log('Аргументы должны быть не меньше нуля');
+    throw new Error('Аргументы должны быть не меньше нуля');
   }
 
   return Math.round(min + Math.random() * (max - min));
@@ -16,11 +16,7 @@ function getRandomInteger (min, max) {
 getRandomInteger(0, 10);
 
 function getStrLength (str, length) {
-  if (str.length - length > 0) {
-    return false;
-  }
-
-  return true;
+  return str.length < length;
 }
 
 getStrLength ('Привет', 10);
