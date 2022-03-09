dcdf2f170a4d5bc0830eec03c922c83ada36ab12
kernelidze <kernelidze@gmail.com>
2022-03-09 20:53:59 +0200

    Исправляем магические числа

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+const SIMILAR_PHOTO_COUNT = 25;
+const COMMENTS_MIN_VALUE = 1;
+const COMMENTS_MAX_VALUE = 10;
+const AVATAR_MIN_VALUE = 1;
+const AVATAR_MAX_VALUE = 6;
+const LIKES_MIN_VALUE = 15;
+const LIKES_MAX_VALUE = 200;
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const NAMES = [
+  'Арнольд',
+  'Василий',
+  'Елисей',
+  'Мария',
+  'Александра'
+];
+
 function getRandomInteger (min, max) {
   if (max < min) {
     const newMax = min;
@@ -19,52 +44,34 @@ getStrLength ('Привет', 10);
 
 const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
-function createIdGenerator () {
-  let lastGeneratedId = 0;
-  return function () {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
+let lastGeneratedPhotoId = 0;
+let lastGeneratedUrlId = 0;
+
+function getPhotoId () {
+  return ++lastGeneratedPhotoId;
 }
 
-const SIMILAR_PHOTO_COUNT = 25;
+function getPhotoUrl () {
+  return ++lastGeneratedUrlId;
+}
 
-const COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const NAMES = [
-  'Арнольд',
-  'Василий',
-  'Елисей',
-  'Мария',
-  'Александра'
-];
-
-const generatePhotoId = createIdGenerator();
-const generateCommentId = createIdGenerator();
-const generatePhotoURL = createIdGenerator();
+const getRandomCommentId = () => getRandomInteger(0, Date.now());
 
 const createComment = () => ({
-  id: generateCommentId(),
-  avatar: getRandomInteger(1, 6),
+  id: getRandomCommentId(),
+  avatar: getRandomInteger(AVATAR_MIN_VALUE, AVATAR_MAX_VALUE),
   message: getRandomArrayElement(COMMENTS),
   name: getRandomArrayElement(NAMES),
 });
 
-const similarCommentsArray = () => Array.from({length: getRandomInteger(1, 10)}, createComment);
+const createSimilarCommentsArray = () => Array.from({length: getRandomInteger(COMMENTS_MIN_VALUE, COMMENTS_MAX_VALUE)}, createComment);
 
 const createPhotoObject = () => ({
-  id : generatePhotoId(),
-  url: `photos/${generatePhotoURL()}.jpg`,
+  id : getPhotoId(),
+  url: `photos/${getPhotoUrl()}.jpg`,
   description: 'Описание фотографии',
-  likes: getRandomInteger(15, 200),
-  comments: similarCommentsArray(),
+  likes: getRandomInteger(LIKES_MIN_VALUE, LIKES_MAX_VALUE),
+  comments: createSimilarCommentsArray(),
 });
 
 const createSimilarPhotoObjects = () => Array.from({length: SIMILAR_PHOTO_COUNT}, createPhotoObject);
